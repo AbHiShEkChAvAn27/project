1dc7f442fdc6325faf2a0bbcd5ec1ab9c068ba92
AbHiShEkChAvAn27 <abhishek.chavan22@pccoepune.org>
2024-09-14 13:51:22 +0000

    r

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import torch
 import torch.nn as nn
 from torchvision import transforms
 import torch.nn.functional as F
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as transforms 
+
+train_path="/workspaces/project/New Plant Diseases Dataset(Augmented)/New Plant Diseases Dataset(Augmented)/train"
+train = ImageFolder(train_path, transform = transforms.ToTensor() )
 
 def accuracy(outputs, labels):    
     _, preds = torch.max(outputs, dim=1)    
@@ -76,31 +81,37 @@ class CNN_NeuralNet(ImageClassificationBase):
         out = self.classifier(out)
         return out
 
-model = torch.load('/workspaces/project/DetectingPlantDiseases.pth', map_location=torch.device('cpu'))
+model = torch.load('/workspaces/project/DetectingPlantDiseases.pth', map_location=torch.device('cpu'), weights_only=True)
 model.eval()
 
-def preprocess_image(image):
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # Standard normalization for pre-trained models
-    ])
-    image = transform(image)
-    image = image.unsqueeze(0)  # Add batch dimension
+device="cpu"
+def to_device(data, device):    
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+def preprocess_image(image):   
+    transform = transforms.Compose([        
+        transforms.ToTensor(),  # Convert image to PyTorch tensor
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # Normalize
+    ])    
+    image = transform(image)        
+    image = image.unsqueeze(0) 
     return image
 
-def predict(image):
+def predict_image(image, model):    
+    image = preprocess_image(image)        
+    xb = to_device(image, device)  
     with torch.no_grad():
-        outputs = model(image)
-        _, predicted = torch.max(outputs, 1)
-    return predicted.item()
+        yb = model(xb)
+        _, preds = torch.max(yb, dim=1)    
+    return train.classes[preds.item()]
 
 st.title('Plant Disease Detection')
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)    
-    image = preprocess_image(image)    
-    prediction = predict(image)    
+    st.image(image, caption='Uploaded Image.', use_column_width=True)        
+    prediction = predict_image(image, model)   
     st.write(f'Predicted class: {prediction}')
